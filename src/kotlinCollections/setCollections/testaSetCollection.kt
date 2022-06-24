4d786f45be76ed82127f4f2e7192db4b44f7ca33
Gabriel <gnunes1909@gmail.com>
2022-06-24 12:50:55 -0300

    Finish 2.03

--- src/kotlinCollections/setCollections/testaSetCollection.kt
+++ src/kotlinCollections/setCollections/testaSetCollection.kt
@@ -1,8 +1,14 @@
 package kotlinCollections.setCollections
 
 fun testaSetCollection() {
-    val assistiramAndroid = listOf("Gabriel", "Ana", "Maria", "Joao")
-    val assistiramKotlin = listOf("Gabriel", "Lia", "Fran", "Joao")
-    val assistiramAmbos = assistiramAndroid + assistiramKotlin
-    println(assistiramAmbos.distinct())
+    val assistiramAndroid = setOf("Gabriel", "Ana", "Maria", "Joao")
+    val assistiramKotlin = setOf("Gabriel", "Lia", "Fran", "Joao")
+
+    val assistiramAmbos: MutableSet<String> = mutableSetOf()
+
+    assistiramAmbos.addAll(assistiramAndroid)
+    assistiramAmbos.addAll(assistiramKotlin)
+    assistiramAmbos.add("Gabriel")
+
+    println(assistiramAmbos)
 }

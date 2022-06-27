81d4bef843eeefa7826e97ddf0eaa0cbfb9e6077
Gabriel <gnunes1909@gmail.com>
2022-06-26 22:16:11 -0300

    Finish 4.03

--- src/kotlinCollections/leituraFiltroMap/testaLeituraFiltrosMap.kt
+++ src/kotlinCollections/leituraFiltroMap/testaLeituraFiltrosMap.kt
@@ -3,7 +3,9 @@ package kotlinCollections.leituraFiltroMap
 fun testaLeituraFiltroMap() {
     val pedidos = mutableMapOf(
         Pair(1, "Produto Teste"),
-        2 to "Produto Teste 2"
+        2 to "Produto Teste 2",
+        4 to "Teste 4",
+        5 to "Teste 5"
     )
 
 //    val getPedido = pedidos.get(3)
@@ -27,20 +29,43 @@ fun testaLeituraFiltroMap() {
 //        println("Valor do pedido: $valor")
 //    }
 
-    val pedidosFiltrados = pedidos.filter { (numero, valor) ->
-        numero % 2 == 0 && valor != ""
-    }
-    println(pedidosFiltrados)
+//    val pedidosFiltrados = pedidos.filter { (numero, valor) ->
+//        numero % 2 == 0 && valor != ""
+//    }
+//    println(pedidosFiltrados)
+//
+//    val keysFiltradas = pedidos.filterKeys { key ->
+//        key <= 2
+//    }
+//    println(keysFiltradas)
+//
+//    val valuesFiltrados = pedidos.filterValues { valor ->
+//        valor != ""
+//    }
+//    println(valuesFiltrados)
 
-    val keysFiltradas = pedidos.filterKeys { key ->
-        key <= 2
-    }
-    println(keysFiltradas)
+//    val novosPedidos = pedidos + Pair(3, "Item 3")
+//    println(novosPedidos)
 
-    val valuesFiltrados = pedidos.filterValues { valor ->
-        valor != ""
-    }
-    println(valuesFiltrados)
+//    val novosPedidos = pedidos + mapOf(4 to "Teste 4", 5 to "Teste 5")
+//    println(novosPedidos)
+//
+//    println(novosPedidos - 1)
+//    println(novosPedidos - listOf(1,2,3))
 
+//    println(pedidos)
+//    pedidos.putAll(setOf(4 to "Teste 4", 5 to "Teste 5"))
+//    println(pedidos)
+
+//    println(pedidos)
+//    pedidos += (setOf(4 to "Teste 4", 5 to "Teste 5"))
+//    println(pedidos)
+
+    println(pedidos)
+    pedidos.keys.remove(1)
+    println(pedidos)
+
+    pedidos.values.remove("Teste 5")
+    println(pedidos)
 }
 

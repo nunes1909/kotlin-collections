32f06fa0909a940dfbb4532a222b599126b4c6b7
Gabriel <gnunes1909@gmail.com>
2022-06-27 19:18:11 -0300

    Finish 5.03

--- src/kotlinCollections/associacaoAgrupamento/testaAssociacaoAgrupamento.kt
+++ src/kotlinCollections/associacaoAgrupamento/testaAssociacaoAgrupamento.kt
@@ -8,21 +8,37 @@ fun testaAssociacaoAgrupamento() {
         Pedido(4, 70.0),
         Pedido(5, 90.5)
     )
-    println("Não mapeados: $pedidos")
+//    println("Não mapeados: $pedidos")
 
     val pedidosMapeados = pedidos.associateBy { pedido ->
         pedido.numero
     }
-    println("Mapeados Associate: $pedidosMapeados")
+//    println("Mapeados Associate: $pedidosMapeados")
 
     val possuiFreteGratis = pedidos.associateWith { pedido ->
         pedido.valor >= 60.0
     }
+//    println("Possui frete gratis? $possuiFreteGratis")
+
     val apenasFreteGratis = possuiFreteGratis.filter { pedidos ->
         pedidos.value
     }
-    println("Possui frete gratis? $possuiFreteGratis")
-    println("Apenas frete gratis: $apenasFreteGratis")
+//    println("Apenas frete gratis filter: $apenasFreteGratis")
+
+    val possuiFreteGratisGroup = pedidos.groupBy { pedido: Pedido ->
+        pedido.valor >= 60.0
+    }
+//    println(possuiFreteGratisGroup)
+//    println(possuiFreteGratisGroup[true])
+
+    val nomes = listOf("Leo", "Bia", "Lia", "Ana", "Joao", "Amanda")
+    println("Nomes: $nomes")
+    val agenda = nomes.groupBy { nomes ->
+        nomes.first()
+    }
+    println("Agenda agrupada por letra: $agenda")
+    println("Nomes com a letra a: ${agenda['A']}")
+
 }
 
 data class Pedido(
